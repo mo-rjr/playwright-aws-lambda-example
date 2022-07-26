001dafe0b178ef825618bad8e6c52021baa1e8b7
Rebecca Rushforth <rebecca.rushforth@metoffice.gov.uk>
2022-07-26 13:34:55 +0100

    Changed loc of npx

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 if [ -z "${AWS_LAMBDA_RUNTIME_API}" ]; then 
   exec /usr/local/bin/aws-lambda-rie /usr/bin/npx aws-lambda-ric $1
 else
-  exec /usr/bin/npx aws-lambda-ric $1
+  exec /usr/local/bin/npx aws-lambda-ric $1
 fi
