6d877aa48fdfbe1521f8e1899af5ad8897dda445
Rebecca Rushforth <rebecca.rushforth@metoffice.gov.uk>
2022-07-26 18:14:23 +0100

    Back to different npx location

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ echo $AWS_LAMBDA_RUNTIME_API
 if [ -z "${AWS_LAMBDA_RUNTIME_API}" ]; then 
   exec /usr/local/bin/aws-lambda-rie /usr/bin/npx aws-lambda-ric $1
 else
-  exec /usr/local/bin/npx aws-lambda-ric $1
+  exec /usr/bin/npx aws-lambda-ric $1
 fi
